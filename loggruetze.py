41a2bbee92ae79625a408bec5083f15f3ec20dd1
Felix J. Ogris <fjo@core7.intra.ogris.net>
2021-05-22 23:16:00 +0200

    lets do frontend with javascript

--- loggruetze.py
+++ loggruetze.py
@@ -1,65 +1,79 @@
 #!/usr/bin/env python3
 
-LOGDIR = "/var/log"
+LOGDIRS = ("/var/log",)
 
-import os, re, datetime
+import sys, os, re, json
 
-archived_log_re = re.compile("\.(\d+)(\.(bz2|gz|xz))?$", re.IGNORECASE)
-
-def bytes_pretty(filesize):
-    for suffix in ("B", "kB", "MB", "GB", "TB"):
-        if filesize < 1024:
-            break
-        filesize /= 1024
-    return "%.2f %s" % (filesize, suffix)
-
-def logfile_sorter(entry):
-    match = archived_log_re.search(entry.name)
-    return -1 if match is None else int(match.group(1))
-
-def traverse_logdir(logdir, subdir="", indent=0):
-    logfiles = []
+def traverse_logdir(logdir, subdir=""):
+    logfiles = {}
 
     try:
         entries = os.scandir(os.path.join(logdir, subdir))
     except Exception as e:
         return logfiles
 
-    entries = filter(lambda entry: not entry.name.startswith("."), entries)
-    entries = sorted(entries, key=logfile_sorter)
-    entries = sorted(entries, key=lambda entry: entry.name)
-
-    oldfile = None
-
     for entry in entries:
         relname = os.path.join(subdir, entry.name)
 
         if entry.is_dir(follow_symlinks=False):
-            print("%s&#128193;%s/" % (" " * indent, entry.name))
-            logfiles += traverse_logdir(logdir, relname, indent + 1)
+            logfiles[entry.path] = {
+                "name"   : entry.name,
+                "childs" : traverse_logdir(logdir, relname)
+            }
         elif entry.is_file(follow_symlinks=False):
-            if oldfile is None or oldfile != archived_log_re.sub("", entry.name):
-                oldfile = entry.name
-                i = indent
-                icon = "&#128196;"
-            else:
-                i = indent + 1
-                icon = "&#128195;"
-
             stat = entry.stat(follow_symlinks=False)
-            print("%s%s%s %s %s" % (" " * i, icon, entry.name, bytes_pretty(stat.st_size), datetime.datetime.fromtimestamp(stat.st_mtime).strftime("%c")))
-            logfiles.append(entry.path)
+            logfiles[entry.path] = {
+                "name"  : entry.name,
+                "mtime" : stat.st_mtime,
+                "size"  : stat.st_size
+            }
 
     return logfiles
 
 
-print("""Content-Type: text/html; charset=utf-8
+logfiles = {}
+for logdir in LOGDIRS:
+    logfiles[logdir] = traverse_logdir(logdir)
 
-<html>
+request_method = os.environ.get("REQUEST_METHOD", "GET")
+
+if request_method == "POST":
+    content_length = int(os.environ.get("CONTENT_LENGTH", "0"))
+    query = json.loads(sys.stdin.read(content_length))
+    action = query.get("action", "")
+
+    if action == "logfiles":
+        result = logfiles
+    elif action == "search":
+        result = { "error" : "not yet implemented" }
+    else:
+        result = { "error" : "unknown action" }
+
+    content_type = "application/json"
+    result = json.dumps(result)
+else:
+    content_type = "text/html; charset=utf-8"
+    result = """<html>
 <head>
 <title>Loggrütze</title>
 </head>
 <body>
-<pre>""")
-logfiles = traverse_logdir(LOGDIR)
-print("</pre></body></html")
+<p>Loggrütze</p>
+<pre id="test">
+</pre>
+<script>
+x = new XMLHttpRequest();
+x.onreadystatechange = function () {
+  if ((x.readyState == 4) && (x.status == 200)) {
+    document.getElementById("test").textContent = x.response;
+  }
+};
+x.open("POST", "");
+x.timeout = 10000;
+x.send('{"action":"logfiles"}');
+</script>
+</body>
+</html>"""
+
+print("Content-Type: %s\nContent-Length: %i\n" % (content_type, len(result)))
+print(result)
